e536aaa291297d76279e31fda82fe40d3076ff2f
Angel <22824872+Zyplos@users.noreply.github.com>
2023-05-11 20:46:22 -0500

    user logout and UI improvements

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ const spinnerElement = document.querySelector(".spinner-icon");
 const notSignedInElement = document.getElementById("not-signed-in-screen");
 const signedInElement = document.getElementById("signed-in-screen");
 const loginButtonElement = document.getElementById("login-button");
+const logoutButtonElement = document.getElementById("logout-button");
+
+const profileContainerElement = document.getElementById("profile-container");
+const profileImageElement = document.getElementById("profile-image");
+const popupElement = document.getElementById("profile-popup");
+const usernameElement = document.getElementById("username");
+
+// set href of login button to url when page loads
+loginButtonElement.href = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&scope=identify%20guilds`;
 
 function hideLoadingSpinner() {
   spinnerElement.classList.add("hidden");
@@ -21,14 +30,85 @@ function hideNotSignedIn() {
   notSignedInElement.classList.add("hidden");
 }
 
+function showNotSignedIn() {
+  notSignedInElement.classList.remove("hidden");
+}
+
 function showSignedIn() {
   signedInElement.classList.remove("hidden");
 }
 
-// redirect to discord login when login button is clicked
-loginButtonElement.addEventListener("click", () => {
-  window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&scope=identify%20guilds`;
+function hideSignedIn() {
+  signedInElement.classList.add("hidden");
+}
+
+function showProfileContainer() {
+  profileContainerElement.classList.remove("hidden");
+}
+
+function hideProfileContainer() {
+  profileContainerElement.classList.add("hidden");
+}
+
+logoutButtonElement.addEventListener("click", () => {
+  logoutUser();
 });
+
+document.addEventListener("click", (event) => {
+  if (!popupElement.contains(event.target) && event.target !== profileImageElement) {
+    popupElement.style.display = "none";
+  }
+});
+
+function logoutUser() {
+  localStorage.removeItem("token");
+  hideProfileContainer();
+  hideSignedIn();
+  showLoginButton();
+  showNotSignedIn();
+
+  // remove all elements in guilds container
+  const guildsContainer = document.getElementById("guilds-container");
+  while (guildsContainer.firstChild) {
+    guildsContainer.removeChild(guildsContainer.firstChild);
+  }
+
+  // remove all elements in features container
+  const featuresContainer = document.getElementById("features-container");
+  while (featuresContainer.firstChild) {
+    featuresContainer.removeChild(featuresContainer.firstChild);
+  }
+
+  getToken();
+}
+
+function getUserProfile(token) {
+  fetch("https://discord.com/api/users/@me", {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      // throw if not ok
+      if (!response.ok) {
+        throw new Error("Not ok");
+      }
+      return response.json();
+    })
+    .then((user) => displayUser(user))
+    .then(() => getGuilds(token))
+    .catch((error) => {
+      console.error(error);
+      localStorage.removeItem("token");
+      getToken();
+    });
+}
+
+function displayUser(user) {
+  profileImageElement.src = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+  usernameElement.textContent = user.username;
+  showProfileContainer();
+}
 
 function getGuilds(token) {
   fetch("https://discord.com/api/users/@me/guilds", {
@@ -59,11 +139,11 @@ function getToken() {
   window.history.replaceState({}, document.title, window.location.pathname);
   if (accessToken) {
     localStorage.setItem("token", accessToken);
-    getGuilds(accessToken);
+    getUserProfile(accessToken);
   } else {
     const savedToken = localStorage.getItem("token");
     if (savedToken) {
-      getGuilds(savedToken);
+      getUserProfile(savedToken);
     } else {
       // just show login button
       hideLoadingSpinner();
@@ -151,17 +231,34 @@ function displayGuilds(guilds) {
       ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`
       : "./unknown-icon.png";
 
+    let guildFeatureElements = "";
+    guild.features.forEach((feature) => {
+      guildFeatureElements += "<span>" + feature + "</span>";
+    });
+
+    let guildPermissionTags = "";
+    if (guild.owner) {
+      guildPermissionTags += '<span class="permissions-tag">OWNER</span>';
+    }
+    if (!guild.owner && (guild.permissions & 0x8) === 0x8) {
+      guildPermissionTags += '<span class="permissions-tag">ADMIN</span>';
+    }
+
     guildInfo.classList.add("guild-info");
     guildInfo.dataset.features = getGuildFeatures(guild).join(",");
     guildInfo.innerHTML = `
-      <h2>${guild.name}</h2>
+      <h2>${guild.name} ${guildPermissionTags}</h2>
       <img src="${iconImageUrl}">
-      <p>Owner: ${guild.owner ? "Yes" : "No"}</p>
-      <p>Features: ${guild.features.join(", ")}</p>
+      <h3>Features</h3>
+      <div class="guild-info-features-container">${guildFeatureElements}</div>
       `;
     guildsContainer.appendChild(guildInfo);
   });
 }
+
+profileImageElement.addEventListener("click", () => {
+  popupElement.style.display = "block";
+});
 
 if (!window.IS_PRODUCTION) {
   new EventSource("/esbuild").addEventListener("change", () => location.reload());
